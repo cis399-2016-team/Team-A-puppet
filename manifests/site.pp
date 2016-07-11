60bcdb2a0b29ef5d895e079fbd2355ae66e28668
VanDeniz <zhibin@cs.uoregon.edu>
2016-07-10 23:25:40 -0700

    added cron jobs to two instances Team-A1 & Team-A2

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,17 @@
 
 node ip-10-0-3-70 {
+	cron { "puppet update":
+		command => "cd /etc/puppet && git pull -q origin master",
+		user    => root,
+		minute  => "*/5",
+	}
 }
 
 node ip-10-0-3-185 {
+	cron { "puppet update":
+		command => "cd /etc/puppet && git pull -q origin master",
+		user    => root,
+		minute  => "*/5",
+	}
 }
 
