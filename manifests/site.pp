2a22e7df3eee457000ecadd33ef8b154b890bd20
VanDeniz <zhibin@cs.uoregon.edu>
2016-07-11 12:49:29 -0700

    updated site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,17 +1,15 @@
-
-node ip-10-0-3-70 {
+node ip-10-0-3-232 {
 	cron { "puppet update":
 		command => "cd /etc/puppet && git pull -q origin master",
 		user    => root,
 		minute  => "*/5",
 	}
 }
+node ip-10-0-3-70 {
+	include "sshd"
+}
 
 node ip-10-0-3-185 {
-	cron { "puppet update":
-		command => "cd /etc/puppet && git pull -q origin master",
-		user    => root,
-		minute  => "*/5",
-	}
+	include "sshd"
 }
 
