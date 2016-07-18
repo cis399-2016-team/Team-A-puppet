0c4ae96631b7fa8d2345b9154b036109291e0b6f
VanDeniz <zhibin@cs.uoregon.edu>
2016-07-18 12:10:29 -0700

    removed webcontent and modified apache/init.pp

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -4,11 +4,26 @@ class apache {
 	}
 
 	file { "/etc/apache2/apache2.conf":
-		mode => 700,
-		owner => "root",
-		group => "root",
+		mode => 644,
+		owner => root,
+		group => root,
 		source => "puppet:///modules/apache/apache2.conf",
 		require => Package["apache2"],
+	}
+
+	file { "/var/www/html":
+		ensure => directory,
+		recurse => true,
+		owner => root,
+		group => root,
+	}
+
+	file { "/var/www/html/foo.html"
+		mode => 644,
+		owner => root,
+		group => root,
+		source => "puppet:///modules/apache/foo.html",
+		require => File["/var/www/html"],
 	}
 
 	service { "apache2":
