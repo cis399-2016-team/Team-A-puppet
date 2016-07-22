ce24b9818dca2bb35437a1616b59c2e5904a4ed3
VanDeniz <zhibin@cs.uoregon.edu>
2016-07-21 17:29:52 -0700

    debugging

--- modules/user-Panda/manifests/init.pp
+++ modules/user-Panda/manifests/init.pp
@@ -1,5 +1,5 @@
 class user-Panda {
-
+	
 	macro-useradd { "Panda":
 		name => "Panda",
 		uid => 1009,
@@ -8,4 +8,3 @@ class user-Panda {
 		sshkey => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDI8Mib9mG3LPmJ+faU6RBnv62pyaWkyHo7qwUnOtuAsKUafy4KhgfNGt1GtA+iuinolK2tGCCPhq4AHoi/uYz35trdDP5BmfSqcDOXf/og6D9AM8Pp/SvrJtab+mKixW25Xzes2r5DTDdne9t4akEdrFu0TX84v+JkoW9k1vffXxwx3d4z22vLoZFkelhGBwdU3QxP5OLBOJLUArSq9xPZ9KC1haFSMgGeUozfg0669EZ8huWKGoR9nyUOo4mFm0M84skDaNOqrEbndWMUXMdotOpigiaU235AcVyygHMMD0WUEQkYK2U9CD+fsnbQYz+GMyEhrvj4WoZhkSjLnJgh"
 	}
 }
-
