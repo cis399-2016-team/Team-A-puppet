174c4a7213706650a2287f7f8d26bff2f1d5c2d3
MicahLevi <mlevi@uoregon.edu>
2016-08-14 15:46:34 -0700

    Added message support for login page

--- modules/php/files/login.php
+++ modules/php/files/login.php
@@ -3,6 +3,14 @@
 <head>
 	<title>Login Page</title>
 </head>
+<body>
+<?php
+
+if (isset($_GET['msg']))
+	echo '<h1>' . $_GET['msg'] . '</h1>';
+
+?>
+
 
 <form action="verify.php" method="POST">
 Username:<br>
@@ -17,7 +25,7 @@ Username:<br>
 <input type="text" name="user" size="60"><br>
 Password:<br>
 <input type="password" name="pass" size="60"><br>
-<input type="submit" value="Log in">
+<input type="submit" value="Create Account">
 </form>
 
 </body>
