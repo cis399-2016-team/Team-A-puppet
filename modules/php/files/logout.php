edd2e841517b32e2bde251178b569841259454b0
VanDeniz <zhibin@cs.uoregon.edu>
2016-08-15 03:59:50 -0700

    update

--- modules/php/files/logout.php
+++ modules/php/files/logout.php
@@ -2,6 +2,6 @@
 	session_start();
 	
 	session_destroy();
-	header("Location:Location: https://ec2-52-36-169-138.us-west-2.compute.amazonaws.com/login.php?msg=Log+out+successfully.");
+	header("Location: https://ec2-52-36-169-138.us-west-2.compute.amazonaws.com/login.php?msg=Log+out+successfully.");
 	exit;
 ?>
