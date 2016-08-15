15aae10544e11e3fecb2fb5239f5af3023c5b8df
VanDeniz <zhibin@cs.uoregon.edu>
2016-08-15 03:14:53 -0700

    update

--- modules/apache-agent2/files/BirdSim.php
+++ modules/apache-agent2/files/BirdSim.php
@@ -2,8 +2,32 @@
    <head>
       <title>Welcome to Bird Sim!</title>
    </head>
-   <applet code="BirdSimPackage/Main.class" 
-	   archive="BirdSim.jar"
-           height=600 width=900> 
-   </applet>
+
+   <?php
+
+	session_start();
+
+	if (isset( $_SESSION['valid'])&& $_SESSION['valid'] == true)
+	{
+    	if ($_SESSION['timeout'] < time() - 300)
+    	{
+        	session_regenerate_id(true);
+        	$_SESSION['timeout'] = time();
+    	}
+	}
+
+	?>
+		<applet code="BirdSimPackage/Main.class" 
+	   			archive="BirdSim.jar"
+           		height=600 width=900> 
+   		</applet>
+
+	<?php
+
+	else
+    {	
+    	header("Location: https://ec2-52-36-169-138.us-west-2.compute.amazonaws.com/login.php?msg=Whoops!+Please+retry+it.");
+    }
+    ?>
+  
 </html>
